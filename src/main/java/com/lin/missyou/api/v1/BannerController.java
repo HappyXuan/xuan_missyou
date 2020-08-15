05b3f447c2def65d55e28c52bd104879ab923dc1
linxuan1103@gmail.com <linxuan1103@gmail.com>
2020-08-15 16:36:17 +0800

    1. missyou构建

---
+++ src/main/java/com/lin/missyou/api/v1/BannerController.java
@@ -0,0 +1,21 @@
+/**
+ * @作者 想做哆啦A梦的程序员
+ * @description: linxuan1103@gmail.com
+ * @创建时间 2020/8/14 15:46
+ */
+package com.lin.missyou.api.v1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/banner")
+public class BannerController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "hello,Doraemon";
+    }
+
+}
