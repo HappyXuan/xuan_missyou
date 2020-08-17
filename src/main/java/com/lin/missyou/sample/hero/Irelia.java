e4816c20abeb33bb1315cfa0cdcd5d3e9ef5c462
linxuan1103@gmail.com <linxuan1103@gmail.com>
2020-08-17 18:03:06 +0800

    1. add SpringBoot基础阶段

---
+++ src/main/java/com/lin/missyou/sample/hero/Irelia.java
@@ -0,0 +1,37 @@
+/**
+ * @作者 想做哆啦A梦的程序员
+ * @description: linxuan1103@gmail.com
+ * @创建时间 2020/8/17 17:45
+ */
+package com.lin.missyou.sample.hero;
+
+import com.lin.missyou.sample.ISkill;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Irelia implements ISkill {
+
+    public Irelia() {
+        System.out.println("Irelia Init");
+    }
+
+    @Override
+    public void q() {
+        System.out.println("Irelia Q");
+    }
+
+    @Override
+    public void w() {
+        System.out.println("Irelia W");
+    }
+
+    @Override
+    public void e() {
+        System.out.println("Irelia E");
+    }
+
+    @Override
+    public void r() {
+        System.out.println("Irelia R");
+    }
+}
